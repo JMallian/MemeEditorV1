0cd5a8e22531562444912676d1d535d5c48b5644
Jessica Mallian <jlmallian@gmail.com>
2018-07-06 15:57:27 -0500

    set up share action and UIImagePickerControllerDelegate

--- MemeEditorV1/ViewController.swift
+++ MemeEditorV1/ViewController.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var takePictureButton: UIBarButtonItem!
     @IBOutlet weak var albumButton: UIBarButtonItem!
     @IBOutlet weak var ShareButton: UIBarButtonItem!
     @IBOutlet weak var CancelButton: UIBarButtonItem!
+    @IBOutlet weak var displayImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func albumButtonSelected(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("user selected image")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("user canceled")
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
