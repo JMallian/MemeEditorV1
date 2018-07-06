b9dbfddcfc0e6af87acc45719e70dae4a1221659
Jessica Mallian <jlmallian@gmail.com>
2018-07-06 15:39:41 -0500

    created some storyboard outlets and most of the layout

--- MemeEditorV1/ViewController.swift
+++ MemeEditorV1/ViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var takePictureButton: UIBarButtonItem!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    @IBOutlet weak var ShareButton: UIBarButtonItem!
+    @IBOutlet weak var CancelButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("I can't wait to push this project to Github!")
     }
 
     override func didReceiveMemoryWarning() {
